564eb2ec45de18de9cf11aa02f66c23cd4c4d2be
Marc Steele <marcsteele2014@gmail.com>
2020-10-09 13:47:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,18 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-
+  example = "test"
+  long_calls = []
+array.detect { |call|
+  if call.length > example.length
+    long_calls.push(call)
+  end
+}
+  if long_calls.length > 0
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
