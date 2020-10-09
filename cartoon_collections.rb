17bb20335704f57ad90bc57de382f05850769560
Marc Steele <marcsteele2014@gmail.com>
2020-10-09 13:48:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ array.detect { |call|
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
